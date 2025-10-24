f000263cc50b3076b116c2a639fcc154beeccb48
Dominic Davies <dominictdavies@gmail.com>
2025-10-24 18:50:04 +0800

    Drawing horizontal lines

--- main.cpp
+++ main.cpp
@@ -6,11 +6,13 @@ static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 
 // Configuration
-const int WIDTH = 840;
-const int HEIGHT = 840;
-const int FPS = 100;
+const int WINDOW_WIDTH = 840;
+const int WINDOW_HEIGHT = 840;
+const int ROW_COUNT = 60;
+const int FPS = 60;
 
 // Derived
+const int ROW_HEIGHT = WINDOW_HEIGHT / ROW_COUNT;
 const int FRAME_DELAY = 1e9 / FPS;
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
@@ -21,13 +23,13 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("Tri-GoL", WIDTH, HEIGHT, 0, &window,
-                                     &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("Tri-GoL", WINDOW_WIDTH, WINDOW_HEIGHT, 0,
+                                     &window, &renderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    SDL_SetRenderLogicalPresentation(renderer, WIDTH, HEIGHT,
+    SDL_SetRenderLogicalPresentation(renderer, WINDOW_WIDTH, WINDOW_HEIGHT,
                                      SDL_LOGICAL_PRESENTATION_LETTERBOX);
 
     return SDL_APP_CONTINUE;
@@ -40,57 +42,26 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
     return SDL_APP_CONTINUE;
 }
 
-/* This function runs once per frame, and is the heart of the program. */
 SDL_AppResult SDL_AppIterate(void *appstate) {
-    int i;
+    // Start with a black canvas
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
 
-    /* Lines (line segments, really) are drawn in terms of points: a set of
-       X and Y coordinates, one set for each end of the line.
-       (0, 0) is the top left of the window, and larger numbers go down
-       and to the right. This isn't how geometry works, but this is pretty
-       standard in 2D graphics. */
-    static const SDL_FPoint line_points[] = {
-        {100, 354}, {220, 230}, {140, 230}, {320, 100}, {500, 230},
-        {420, 230}, {540, 354}, {400, 354}, {100, 354}};
-
-    /* as you can see from this, rendering draws over whatever was drawn before
-     * it. */
-    SDL_SetRenderDrawColor(renderer, 100, 100, 100,
-                           SDL_ALPHA_OPAQUE); /* grey, full alpha */
-    SDL_RenderClear(renderer);                /* start with a blank canvas. */
-
-    /* You can draw lines, one at a time, like these brown ones... */
-    SDL_SetRenderDrawColor(renderer, 127, 49, 32, SDL_ALPHA_OPAQUE);
-    SDL_RenderLine(renderer, 240, 450, 400, 450);
-    SDL_RenderLine(renderer, 240, 356, 400, 356);
-    SDL_RenderLine(renderer, 240, 356, 240, 450);
-    SDL_RenderLine(renderer, 400, 356, 400, 450);
-
-    /* You can also draw a series of connected lines in a single batch... */
-    SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
-    SDL_RenderLines(renderer, line_points, SDL_arraysize(line_points));
-
-    /* here's a bunch of lines drawn out from a center point in a circle. */
-    /* we randomize the color of each line, so it functions as animation. */
-    for (i = 0; i < 360; i++) {
-        const float size = 30.0f;
-        const float x = 320.0f;
-        const float y = 95.0f - (size / 2.0f);
-        SDL_SetRenderDrawColor(renderer, SDL_rand(256), SDL_rand(256),
-                               SDL_rand(256), SDL_ALPHA_OPAQUE);
-        SDL_RenderLine(renderer, x, y, x + SDL_sinf((float)i) * size,
-                       y + SDL_cosf((float)i) * size);
+    // Draw grey lines
+    SDL_SetRenderDrawColor(renderer, 64, 64, 64, SDL_ALPHA_OPAQUE);
+    for (int y = ROW_HEIGHT; y < WINDOW_HEIGHT; y += ROW_HEIGHT) {
+        SDL_RenderLine(renderer, 0, y, WINDOW_WIDTH, y);
     }
 
-    SDL_RenderPresent(renderer); /* put it all on the screen! */
+    // Put everything onto the screen
+    SDL_RenderPresent(renderer);
 
     // Wait before next frame
     SDL_DelayPrecise(FRAME_DELAY);
 
-    return SDL_APP_CONTINUE; /* carry on with the program! */
+    return SDL_APP_CONTINUE;
 }
 
-/* This function runs once at shutdown. */
 void SDL_AppQuit(void *appstate, SDL_AppResult result) {
-    /* SDL will clean up the window/renderer for us. */
+    // SDL will clean up the window/renderer
 }
