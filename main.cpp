c032602384c38cc6502625280f5f8f3d3af9c901
Dominic Davies <dominictdavies@gmail.com>
2025-10-29 23:31:39 +0800

    Add `COLUMN_COUNT` and reorder

--- main.cpp
+++ main.cpp
@@ -1,10 +1,8 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <cmath>
 #include <numbers>
-
-static SDL_Window *window = nullptr;
-static SDL_Renderer *renderer = nullptr;
 
 // Configuration
 constexpr unsigned WINDOW_WIDTH = 840;
@@ -17,7 +15,30 @@ constexpr bool DRAW_GRID = true;
 constexpr unsigned FRAME_DELAY = 1e9 / FPS;
 constexpr double ROW_HEIGHT = static_cast<double>(WINDOW_HEIGHT) / ROW_COUNT;
 constexpr double COLUMN_WIDTH = 2 * ROW_HEIGHT / std::numbers::sqrt3;
+constexpr unsigned COLUMN_COUNT = std::ceil(WINDOW_WIDTH / COLUMN_WIDTH);
 constexpr double DIAGONAL_LINE_TRAVEL_X = COLUMN_WIDTH / 2 * ROW_COUNT;
+
+static SDL_Window *window = nullptr;
+static SDL_Renderer *renderer = nullptr;
+
+void draw_grid() {
+    // Use opaque grey colour
+    SDL_SetRenderDrawColor(renderer, 64, 64, 64, SDL_ALPHA_OPAQUE);
+
+    // Draw horizontal lines
+    for (double y = ROW_HEIGHT; y < WINDOW_HEIGHT; y += ROW_HEIGHT) {
+        SDL_RenderLine(renderer, 0, y, WINDOW_WIDTH, y);
+    }
+
+    // Draw diagonal lines
+    for (double x = -DIAGONAL_LINE_TRAVEL_X;
+         x < WINDOW_WIDTH + DIAGONAL_LINE_TRAVEL_X; x += COLUMN_WIDTH) {
+        SDL_RenderLine(renderer, x, 0, x - DIAGONAL_LINE_TRAVEL_X,
+                       WINDOW_HEIGHT);
+        SDL_RenderLine(renderer, x, 0, x + DIAGONAL_LINE_TRAVEL_X,
+                       WINDOW_HEIGHT);
+    }
+}
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
     SDL_SetAppMetadata("Tri-GoL", "0.1", "dev.dominictdavies.trigol");
@@ -44,25 +65,6 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
         return SDL_APP_SUCCESS;
     }
     return SDL_APP_CONTINUE;
-}
-
-void draw_grid() {
-    // Use opaque grey colour
-    SDL_SetRenderDrawColor(renderer, 64, 64, 64, SDL_ALPHA_OPAQUE);
-
-    // Draw horizontal lines
-    for (double y = ROW_HEIGHT; y < WINDOW_HEIGHT; y += ROW_HEIGHT) {
-        SDL_RenderLine(renderer, 0, y, WINDOW_WIDTH, y);
-    }
-
-    // Draw diagonal lines
-    for (double x = -DIAGONAL_LINE_TRAVEL_X;
-         x < WINDOW_WIDTH + DIAGONAL_LINE_TRAVEL_X; x += COLUMN_WIDTH) {
-        SDL_RenderLine(renderer, x, 0, x - DIAGONAL_LINE_TRAVEL_X,
-                       WINDOW_HEIGHT);
-        SDL_RenderLine(renderer, x, 0, x + DIAGONAL_LINE_TRAVEL_X,
-                       WINDOW_HEIGHT);
-    }
 }
 
 SDL_AppResult SDL_AppIterate(void *appstate) {
