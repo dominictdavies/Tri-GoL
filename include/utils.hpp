3430226e7a44da0aa1a2584472fc0cdac7f1b1ee
Dominic Davies <dominictdavies@gmail.com>
2025-11-05 12:23:50 +0800

    Pass bitsets by reference

--- include/utils.hpp
+++ include/utils.hpp
@@ -5,10 +5,10 @@
 #include <bitset>
 #include <cstdint>
 
-uint8_t get_neighbourhood(const std::bitset<CELL_COUNT> is_alive, unsigned row,
+uint8_t get_neighbourhood(const std::bitset<CELL_COUNT> &is_alive, unsigned row,
                           unsigned col);
 
-std::bitset<CELL_COUNT> execute_rule(const std::bitset<CELL_COUNT> is_alive,
+std::bitset<CELL_COUNT> execute_rule(const std::bitset<CELL_COUNT> &is_alive,
                                      uint16_t rule = 32368);
 
 #endif
