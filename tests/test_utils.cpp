4d3aa57bf390ff7de5f81c8c9c2c1114c22ec622
Dominic Davies <dominictdavies@gmail.com>
2025-11-09 16:28:52 +0800

    Cast for test readability

--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -11,46 +11,53 @@ TEST_CASE("gets all neighbourhoods correctly", "[get_neighbourhood]") {
     unsigned down_row = is_up_triangle ? MIDDLE_ROW + 1 : MIDDLE_ROW - 1;
 
     SECTION("dead-empty neighbourhood is 0") {
-        REQUIRE(get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL) == 0);
+        REQUIRE(static_cast<unsigned>(
+                    get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL)) == 0);
     }
 
     SECTION("dead-left neighbourhood is 1") {
         is_alive.set(MIDDLE_ROW * COL_COUNT + left_col);
 
-        REQUIRE(get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL) == 1);
+        REQUIRE(static_cast<unsigned>(
+                    get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL)) == 1);
     }
 
     SECTION("dead-right neighbourhood is 2") {
         is_alive.set(MIDDLE_ROW * COL_COUNT + right_col);
 
-        REQUIRE(get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL) == 2);
+        REQUIRE(static_cast<unsigned>(
+                    get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL)) == 2);
     }
 
     SECTION("dead-down neighbourhood is 3") {
         is_alive.set(down_row * COL_COUNT + MIDDLE_COL);
 
-        REQUIRE(get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL) == 3);
+        REQUIRE(static_cast<unsigned>(
+                    get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL)) == 3);
     }
 
     SECTION("dead-left-right neighbourhood is 4") {
         is_alive.set(MIDDLE_ROW * COL_COUNT + left_col);
         is_alive.set(MIDDLE_ROW * COL_COUNT + right_col);
 
-        REQUIRE(get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL) == 4);
+        REQUIRE(static_cast<unsigned>(
+                    get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL)) == 4);
     }
 
     SECTION("dead-left-down neighbourhood is 5") {
         is_alive.set(MIDDLE_ROW * COL_COUNT + left_col);
         is_alive.set(down_row * COL_COUNT + MIDDLE_COL);
 
-        REQUIRE(get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL) == 5);
+        REQUIRE(static_cast<unsigned>(
+                    get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL)) == 5);
     }
 
     SECTION("dead-right-down neighbourhood is 6") {
         is_alive.set(MIDDLE_ROW * COL_COUNT + right_col);
         is_alive.set(down_row * COL_COUNT + MIDDLE_COL);
 
-        REQUIRE(get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL) == 6);
+        REQUIRE(static_cast<unsigned>(
+                    get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL)) == 6);
     }
 
     SECTION("dead-full neighbourhood is 7") {
@@ -58,34 +65,39 @@ TEST_CASE("gets all neighbourhoods correctly", "[get_neighbourhood]") {
         is_alive.set(MIDDLE_ROW * COL_COUNT + right_col);
         is_alive.set(down_row * COL_COUNT + MIDDLE_COL);
 
-        REQUIRE(get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL) == 7);
+        REQUIRE(static_cast<unsigned>(
+                    get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL)) == 7);
     }
 
     SECTION("live-empty neighbourhood is 8") {
         is_alive.set(MIDDLE_ROW * COL_COUNT + MIDDLE_COL);
 
-        REQUIRE(get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL) == 8);
+        REQUIRE(static_cast<unsigned>(
+                    get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL)) == 8);
     }
 
     SECTION("live-left neighbourhood is 9") {
         is_alive.set(MIDDLE_ROW * COL_COUNT + MIDDLE_COL);
         is_alive.set(MIDDLE_ROW * COL_COUNT + left_col);
 
-        REQUIRE(get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL) == 9);
+        REQUIRE(static_cast<unsigned>(
+                    get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL)) == 9);
     }
 
     SECTION("live-right neighbourhood is 10") {
         is_alive.set(MIDDLE_ROW * COL_COUNT + MIDDLE_COL);
         is_alive.set(MIDDLE_ROW * COL_COUNT + right_col);
 
-        REQUIRE(get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL) == 10);
+        REQUIRE(static_cast<unsigned>(
+                    get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL)) == 10);
     }
 
     SECTION("live-down neighbourhood is 11") {
         is_alive.set(MIDDLE_ROW * COL_COUNT + MIDDLE_COL);
         is_alive.set(down_row * COL_COUNT + MIDDLE_COL);
 
-        REQUIRE(get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL) == 11);
+        REQUIRE(static_cast<unsigned>(
+                    get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL)) == 11);
     }
 
     SECTION("live-left-right neighbourhood is 12") {
@@ -93,7 +105,8 @@ TEST_CASE("gets all neighbourhoods correctly", "[get_neighbourhood]") {
         is_alive.set(MIDDLE_ROW * COL_COUNT + left_col);
         is_alive.set(MIDDLE_ROW * COL_COUNT + right_col);
 
-        REQUIRE(get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL) == 12);
+        REQUIRE(static_cast<unsigned>(
+                    get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL)) == 12);
     }
 
     SECTION("live-left-down neighbourhood is 13") {
@@ -101,7 +114,8 @@ TEST_CASE("gets all neighbourhoods correctly", "[get_neighbourhood]") {
         is_alive.set(MIDDLE_ROW * COL_COUNT + left_col);
         is_alive.set(down_row * COL_COUNT + MIDDLE_COL);
 
-        REQUIRE(get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL) == 13);
+        REQUIRE(static_cast<unsigned>(
+                    get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL)) == 13);
     }
 
     SECTION("live-right-down neighbourhood is 14") {
@@ -109,7 +123,8 @@ TEST_CASE("gets all neighbourhoods correctly", "[get_neighbourhood]") {
         is_alive.set(MIDDLE_ROW * COL_COUNT + right_col);
         is_alive.set(down_row * COL_COUNT + MIDDLE_COL);
 
-        REQUIRE(get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL) == 14);
+        REQUIRE(static_cast<unsigned>(
+                    get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL)) == 14);
     }
 
     SECTION("live-full neighbourhood is 15") {
@@ -118,7 +133,8 @@ TEST_CASE("gets all neighbourhoods correctly", "[get_neighbourhood]") {
         is_alive.set(MIDDLE_ROW * COL_COUNT + right_col);
         is_alive.set(down_row * COL_COUNT + MIDDLE_COL);
 
-        REQUIRE(get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL) == 15);
+        REQUIRE(static_cast<unsigned>(
+                    get_neighbourhood(is_alive, MIDDLE_ROW, MIDDLE_COL)) == 15);
     }
 }
 
